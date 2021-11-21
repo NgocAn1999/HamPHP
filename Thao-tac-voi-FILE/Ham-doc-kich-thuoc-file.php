bd904e42917c2ee73a013257541dcfab044ff762
Đặng Ngọc An <dangngocan8686@gmail.com>
2021-11-21 11:44:03 +0700

    Update Ham-doc-kich-thuoc-file.php

--- Thao-tac-voi-FILE/Ham-doc-kich-thuoc-file.php
+++ Thao-tac-voi-FILE/Ham-doc-kich-thuoc-file.php
@@ -1,7 +1,18 @@
 <?php
     // Hàm đọc kích thước FILE
-    function converSize(){
+    function converSize($size,$totalDigit=2,$ditance=' '){
         $units=array('B','KB','MB','GB','TB');
-    }
+        $length=count($units);
 
+        for($i=0;$i<$length;$i++){
+            if($size>1024){
+                $size=$size/1024;
+            }else{
+                $unit =$units[$i];
+                break;
+            }
+        }
+        $result= round($size,$totalDigit).$ditance.$unit;
+        return $result;
+    }
 ?>
